b7c3a0faee37d9c9ea3f719e2f11fddc79020d32
nmkhans <nurmoin05@gmail.com>
2024-06-15 22:23:58 +0600

    models created and migerated

--- author/models.py
+++ author/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+  name = models.CharField(max_length = 50)
+  bio = models.TextField()
+  phone_no = models.IntegerField(max_length = 11)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from categories.models import Categories
+from author.models import Author
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length = 100)
+  content = models.TextField()
+  categories = models.ManyToManyField(Categories) 
+  # one post has many categories and one category has many post (many to many relation)
+  author = models.ForeignKey(Author, on_delete = models.CASCADE)
+  # one author has many post but a post has one author (one to many relation)
