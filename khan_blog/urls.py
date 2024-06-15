c2192aeb09b35a57f3bac2d1c94c995d23af1d4c
nmkhans <nurmoin05@gmail.com>
2024-06-15 17:08:25 +0600

    url patterns added to all and and home page setted up

--- khan_blog/urls.py
+++ khan_blog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name = 'home'),
     path('author/', include('author.urls')),
     path('categories/', include('categories.urls')),
     path('post/', include('post.urls')),
