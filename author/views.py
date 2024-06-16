2c7eb5514e3394329b7c1a8a23c59584a7b9d928
nmkhans <nurmoin05@gmail.com>
2024-06-16 13:03:07 +0600

    add author feature complete

--- author/views.py
+++ author/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AuthorForm
 
 # Create your views here.
 def add_author(req):
-  return render(req, 'author/add_author.html')
+  if req.method == 'POST':
+    form = AuthorForm(req.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = AuthorForm()
+
+  return render(req, 'author/add_author.html', {'form': form})
